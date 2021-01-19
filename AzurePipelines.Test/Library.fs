a9121c435a841a21ee4e3708eb50fcea468d203f
Ryan Wennekes <ryanwennekes@gmail.com>
2021-01-19 12:37:33 +0100

    Added Test Library

---
+++ AzurePipelines.Test/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace AzurePipelines.Test
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ AzurePipelines.TestLibrary/MyTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace AzurePipelines.TestLibrary
+{
+    public class MyTest
+    {
+        [Fact]
+        public void Succeed()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Fail()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
